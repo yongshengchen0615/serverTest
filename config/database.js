c311bf886dbaf9515bdc02c1b1c895427ea2ce63
yongshengchen0615 <yongshengchen.tw@gmail.com>
2025-03-19 14:47:47 +0800

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://yongsheng:L3eRBm2rUnD6fMJE@cluster0.lreob.mongodb.net/usersDB?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
