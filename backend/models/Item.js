b2d7ad7a3ae281a294b35d8c37043b4b3266f692
yongshengchen0615 <yongshengchen.tw@gmail.com>
2025-03-20 12:52:33 +0800

    Delete backend directory

--- backend/models/Item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-// 定義 Item 資料表結構
-const ItemSchema = new mongoose.Schema({
-    name: String,
-    phone: String
-});
-
-module.exports = mongoose.model("Item", ItemSchema);
